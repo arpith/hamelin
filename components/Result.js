48ef813bc185b6ab621cd336282ee1b05e14c5a3
Arpith Siromoney <arpith@feedreader.co>
2016-02-25 11:41:47 +0530

    move link style to result comp

--- components/Result.js
+++ components/Result.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router';
 
 class Result extends React.Component {
   render() {
-    let style = {
+    const itemStyle = {
       backgroundImage: 'url(' + this.props.result.thumbnail + ')',
       listStyle: 'none',
       display: 'block',
@@ -11,6 +11,13 @@ class Result extends React.Component {
       height: '240px',
       filter: 'blur(5px)'
     };
+
+    const linkStyle = {
+      textDecoration: 'none',
+      color: 'white',
+      textTransform: 'uppercase',
+    };
+
     return (
       <li style={style}><Link to={'/' + this.props.result.id}>{this.props.result.title}</Link></li>
     );
