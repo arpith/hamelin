219cb3359156734b379ba4369b46bf40bfb778d9
Arpith Siromoney <arpith@feedreader.co>
2016-02-26 13:29:21 +0530

    use bebas for links

--- components/Result.js
+++ components/Result.js
@@ -21,6 +21,7 @@ class Result extends React.Component {
     };
 
     const linkStyle = {
+      fontFamily: 'Bebas Neue Bold',
       display: 'block',
       textDecoration: 'none',
       color: 'white',
