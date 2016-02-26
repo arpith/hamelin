1b8f8438c14ad27a0fbafd52b2af6880f8cbe2c5
Arpith Siromoney <arpith@feedreader.co>
2016-02-26 14:09:08 +0530

    move result title to separate component

--- components/Result.js
+++ components/Result.js
@@ -1,28 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router';
 
+import Title from './Title';
 import Thumbnail from './Thumbnail';
 
 class Result extends React.Component {
   render() {
     const style = {
-      fontFamily: 'Bebas Neue Bold',
-      fontSize: '2em',
-      lineHeight: 'inherit',
       display: 'block',
       textDecoration: 'none',
-      color: 'white',
-      textTransform: 'uppercase',
       position: 'relative',
       height: 180,
       width: 320,
-      margin: 20,
+      margin: 10,
       float: 'left'
     };
 
     return (
       <Link to={'/' + this.props.result.id} style={style}>
-        {this.props.result.title}
+        <Title text={this.props.result.title} />
         <Thumbnail url={this.props.result.thumbnail} />
       </Link>
     );
