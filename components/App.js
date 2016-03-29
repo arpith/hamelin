690fafefe8ee98408376800bb1ee2d9fcc5f5cb4
Arpith Siromoney <arpith@feedreader.co>
2016-03-29 11:29:57 +0530

    style title

--- components/App.js
+++ components/App.js
@@ -6,9 +6,18 @@ import ResultList from './ResultList';
  
 class App extends React.Component {
   render() {
+    const style = {
+      padding: 10,
+      fontFamily: 'Bebas Neue Bold',
+      fontSize: '2em',
+      lineHeight: 'inherit',
+      color: 'black',
+      textTransform: 'uppercase',
+    };
+
     return (
       <div>
-        <IndexLink to='/'><h1>Hamelin</h1></IndexLink>
+        <IndexLink to='/'><h1 style={style}>Hamelin</h1></IndexLink>
         {this.props.children}
         <Search />
         <ResultList />
